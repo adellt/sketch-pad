459cbde8d4fcec7baefe046d4c4d2ab53988130c
adellt <adell568@gmail.com>
2014-08-05 15:37:46 +1000

    Update

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-//function init() {
-//	for (var i = 1; i <= 16; i++) {
-//		
-//};
-//
+function init() {
+	for (var i = 1; i <= 16; i++) {
+		$("#wrapper").append("<div class='box'>Box</div>");
+	}
+};
+
