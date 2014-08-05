b22e80fdf5e8c76ab21fef41980c899cfde8bb20
adellt <adell568@gmail.com>
2014-08-05 23:05:27 +1000

    Boxes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function init() {
-	for (var i = 1; i <= 16; i++) {
-		$("#wrapper").append("<div class='box'>Box</div>");
-	}
-};
-
+$(document).ready(function(){
+	$('#go').click(function() {
+		for (var i = 1; i <= 16; i++) {
+			$("#wrapper").append("<div class='box'>Box</div>");
+		}
+	});
+})
