fc618c76a426dcdbfdeaf8f702ccffa701144da3
adellt <adell568@gmail.com>
2014-08-06 21:57:08 +1000

    Trying fade

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 $(document).ready(function(){
+	
+	$('#solid').click(function() {
 
-	$("#buttons").on("mouseenter", function(){
-		$(this).addClass("trail");
-	});
+		var number = $("#number").val();
+		$("#wrapper").html("");
 
-	$(".bix").on("mouseenter", function(){
-		$(this).addClass("trail");
-	});
-
-
-
-	$('#go').click(function() {
-		for (var i = 1; i <= 16; i++) {
-			$("#wrapper").append("<div class='box'>Box</div>");
+		for (var i = 1; i <= (number*number); i++) {
+			$("#wrapper").append("<div class='box'>&nbsp;</div>");
 		}
+
+		var size = parseInt($("#wrapper").css("width"),10) / parseInt(number, 10) + "px";
+		$(".box").css({"width":size, "height":size});
+
+		$(".box").on("mouseover", function(){
+			$(this).addClass("trail");
+		});
 	});
 
 
+	$('#fade').click(function() {
+		
+		var number = $("#number").val();
+		$("#wrapper").html("");
+
+		for (var i = 1; i <= (number*number); i++) {
+			$("#wrapper").append("<div class='box'>&nbsp;</div>");
+		}
+
+		var size = parseInt($("#wrapper").css("width"),10) / parseInt(number, 10) + "px";
+		$(".box").css({"width":size, "height":size});
+		$(".box").css("opacity":"100");
+
+		$(".box").on("mouseover", function(){
+			var opacity = $(".box").css("opacity");
+			if ()
+			$(this).css("opacity", opacity);
+			//reduce opacity by 10 every time
+		});
+	});
 
 
 })
