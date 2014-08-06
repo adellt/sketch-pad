35ede8bbf604086b319cf29a3264e14c0360f649
adellt <adell568@gmail.com>
2014-08-06 16:01:47 +1000

    Trying mouseenter

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
+
+	$("#buttons").on("mouseenter", function(){
+		$(this).addClass("trail");
+	});
+
+	$(".bix").on("mouseenter", function(){
+		$(this).addClass("trail");
+	});
+
+
+
 	$('#go').click(function() {
 		for (var i = 1; i <= 16; i++) {
 			$("#wrapper").append("<div class='box'>Box</div>");
 		}
 	});
+
+
+
+
 })
