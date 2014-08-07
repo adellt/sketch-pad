38ac75fdca19a929841e18bd17d956a241c19ddf
adellt <adell568@gmail.com>
2014-08-07 14:32:49 +1000

    Before function reduction

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 		$(".box").css({"width":size, "height":size});
 
 		$(".box").on("mouseover", function(){
-			$(this).addClass("trail");
+			$(this).addClass("solid");
 		});
 	});
 	
@@ -35,15 +35,39 @@ $(document).ready(function(){
 			var opacity = $(".box").css("opacity");
 			
 			if (opacity === 0) {
-				opacity = 0;
+				opacity = opacity+0;
 			} else {
 				opacity = opacity-0.1;
 			};
 						
-			$(this).css("opacity", opacity);//Is not changing opacity, but is adding trail
+			$(this).css("opacity", opacity);
 		});
 	});
 
+
+	$('#trail').click(function() {
+
+		var number = $("#number").val();
+		$("#wrapper").html("");
+
+		for (var i = 1; i <= (number*number); i++) {
+			$("#wrapper").append("<div class='box'>&nbsp;</div>");
+		}
+
+		var size = parseInt($("#wrapper").css("width"),10) / parseInt(number, 10) + "px";
+		$(".box").css({"width":size, "height":size});
+
+		$(".box").on("mouseover", function(){
+			$(this).animate({
+				"background-color":"#F50C33", 
+			},200);
+
+			$(this).animate({
+				"opacity":0.0
+			},500);
+			
+		});
+	});
 
 
 })
